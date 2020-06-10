43c5c31cab44a95933612a871550f8ff9ad90170
Mateusz Slyszewski <mateuszslyszewski@gmail.com>
2020-06-10 15:39:14 +0200

    fetch method

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,20 @@ import React, { Component } from 'react';
 import Form from './Form';
 import Result from './Reults';
 import './App.css';
+const APIKey = '78c3aee290a22883e08a12dcc07f433e';
 
 class App extends Component {
 
   state = {
-    value: ""
+    value: '',
+    date: '',
+    city: '',
+    sunrise: '',
+    sunset: '',
+    temp: '',
+    pressure: '',
+    wind: '',
+    err: false,
   }
 
   handleInputChange = (e) => {
@@ -15,10 +24,29 @@ class App extends Component {
     })
   }
 
+  handleCitySubmit = (e) => {
+    e.preventDefault();
+    const API = `http://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&APPID=${APIKey}&units=metric`
+
+    fetch(API)
+      .then(response => {
+        if (response.ok) {
+          return response
+        }
+        throw Error("Nie udało się")
+      })
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(err => console.log(err))
+  }
+
   render() {
     return (
       <div>
-        <Form value={this.state.value} change={this.handleInputChange} />
+        <Form
+          value={this.state.value}
+          change={this.handleInputChange}
+          submit={this.handleCitySubmit} />
         <Result />
       </div>
     );
--- src/components/Form.js
+++ src/components/Form.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Form = props => {
     return (
-        <form>
+        <form onSubmit={props.submit}>
             <input
                 type="text"
                 value={props.value}
